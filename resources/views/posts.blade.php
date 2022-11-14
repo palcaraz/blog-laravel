b3c63e82e7e2a8ab07702fe9fc094a5d7876f030
Pedro Alcaraz <palcaraz@gmail.com>
2022-11-14 22:30:24 +0100

    Dynamics links to html files

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,36 +1,72 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=1024, initial-scale=1.0">
-    <title>My Blog</title>
-    <link rel="stylesheet" href="/app.css">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=1024, initial-scale=1.0">
+  <title>My Blog</title>
+  <link rel="stylesheet" href="/app.css">
 </head>
+
 <body>
-    <article>
-        <h1><a href="/post">My First Post</a></h1>
+  <article>
+    <h1><a href="/posts/my-first-post">My First Post</a></h1>
 
-        <p>
-          Lorem ipsum dolor sit amet, conectetur adipiscing elit. Morbi viverra vehicula nisl eget blandit. Mauris hendrerit accumsan est id cursus. Ut sed elit at ligula tempor porta sed sed odio. Aenean eget tortor purus. Donec vulputate nisl ut arcu eleifend, non tempor neque aliquam. Nunc dignissim augue eget enim vulputate, non vulputate velit scelerisque. Vivamus hendrerit ipsum mi, non sagittis est tincidunt quis. Aliquam sed nisl eget lectus scelerisque sagittis a consectetur ex. Duis congue, massa vel tincidunt vestibulum, mi eros luctus lacus, id malesuada sem dui molestie sapien. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut orci velit, viverra at leo ut, convallis lobortis libero. Cras hendrerit sed tortor vitae feugiat. Proin et arcu risus. Phasellus lacinia ullamcorper convallis.
-        </p>
-    </article>
+    <p>
+      Lorem ipsum dolor sit amet, conectetur adipiscing elit. Morbi viverra
+      vehicula nisl eget blandit. Mauris hendrerit accumsan est id cursus.
+      Ut sed elit at ligula tempor porta sed sed odio. Aenean eget tortor
+      purus. Donec vulputate nisl ut arcu eleifend, non tempor neque
+      aliquam. Nunc dignissim augue eget enim vulputate, non vulputate velit
+      scelerisque. Vivamus hendrerit ipsum mi, non sagittis est tincidunt
+      quis. Aliquam sed nisl eget lectus scelerisque sagittis a consectetur
+      ex. Duis congue, massa vel tincidunt vestibulum, mi eros luctus lacus,
+      id malesuada sem dui molestie sapien. Orci varius natoque penatibus et
+      magnis dis parturient montes, nascetur ridiculus mus. Ut orci velit,
+      viverra at leo ut, convallis lobortis libero. Cras hendrerit sed
+      tortor vitae feugiat. Proin et arcu risus. Phasellus lacinia
+      ullamcorper convallis. </p>
+  </article>
 
-    <article>
-        <h1><a href="/post">My Second Post</a></h1>
+  <article>
+    <h1><a href="/posts/my-second-post">My Second Post</a></h1>
 
-        <p>
-          Lorem ipsum dolor sit amet, conectetur adipiscing elit. Morbi viverra vehicula nisl eget blandit. Mauris hendrerit accumsan est id cursus. Ut sed elit at ligula tempor porta sed sed odio. Aenean eget tortor purus. Donec vulputate nisl ut arcu eleifend, non tempor neque aliquam. Nunc dignissim augue eget enim vulputate, non vulputate velit scelerisque. Vivamus hendrerit ipsum mi, non sagittis est tincidunt quis. Aliquam sed nisl eget lectus scelerisque sagittis a consectetur ex. Duis congue, massa vel tincidunt vestibulum, mi eros luctus lacus, id malesuada sem dui molestie sapien. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut orci velit, viverra at leo ut, convallis lobortis libero. Cras hendrerit sed tortor vitae feugiat. Proin et arcu risus. Phasellus lacinia ullamcorper convallis.
-        </p>
-    </article>
+    <p>
+      Lorem ipsum dolor sit amet, conectetur adipiscing elit. Morbi viverra
+      vehicula nisl eget blandit. Mauris hendrerit accumsan est id cursus.
+      Ut sed elit at ligula tempor porta sed sed odio. Aenean eget tortor
+      purus. Donec vulputate nisl ut arcu eleifend, non tempor neque
+      aliquam. Nunc dignissim augue eget enim vulputate, non vulputate velit
+      scelerisque. Vivamus hendrerit ipsum mi, non sagittis est tincidunt
+      quis. Aliquam sed nisl eget lectus scelerisque sagittis a consectetur
+      ex. Duis congue, massa vel tincidunt vestibulum, mi eros luctus lacus,
+      id malesuada sem dui molestie sapien. Orci varius natoque penatibus et
+      magnis dis parturient montes, nascetur ridiculus mus. Ut orci velit,
+      viverra at leo ut, convallis lobortis libero. Cras hendrerit sed
+      tortor vitae feugiat. Proin et arcu risus. Phasellus lacinia
+      ullamcorper convallis. </p>
+  </article>
 
-    <article>
-        <h1><a href="/post">My Third Post</a></h1>
+  <article>
+    <h1><a href="/posts/my-third-post">My Third Post</a></h1>
 
-        <p>
-          Lorem ipsum dolor sit amet, conectetur adipiscing elit. Morbi viverra vehicula nisl eget blandit. Mauris hendrerit accumsan est id cursus. Ut sed elit at ligula tempor porta sed sed odio. Aenean eget tortor purus. Donec vulputate nisl ut arcu eleifend, non tempor neque aliquam. Nunc dignissim augue eget enim vulputate, non vulputate velit scelerisque. Vivamus hendrerit ipsum mi, non sagittis est tincidunt quis. Aliquam sed nisl eget lectus scelerisque sagittis a consectetur ex. Duis congue, massa vel tincidunt vestibulum, mi eros luctus lacus, id malesuada sem dui molestie sapien. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut orci velit, viverra at leo ut, convallis lobortis libero. Cras hendrerit sed tortor vitae feugiat. Proin et arcu risus. Phasellus lacinia ullamcorper convallis.
-        </p>
-    </article>
+    <p>
+      Lorem ipsum dolor sit amet, conectetur adipiscing elit. Morbi viverra
+      vehicula nisl eget blandit. Mauris hendrerit accumsan est id cursus.
+      Ut sed elit at ligula tempor porta sed sed odio. Aenean eget tortor
+      purus. Donec vulputate nisl ut arcu eleifend, non tempor neque
+      aliquam. Nunc dignissim augue eget enim vulputate, non vulputate velit
+      scelerisque. Vivamus hendrerit ipsum mi, non sagittis est tincidunt
+      quis. Aliquam sed nisl eget lectus scelerisque sagittis a consectetur
+      ex. Duis congue, massa vel tincidunt vestibulum, mi eros luctus lacus,
+      id malesuada sem dui molestie sapien. Orci varius natoque penatibus et
+      magnis dis parturient montes, nascetur ridiculus mus. Ut orci velit,
+      viverra at leo ut, convallis lobortis libero. Cras hendrerit sed
+      tortor vitae feugiat. Proin et arcu risus. Phasellus lacinia
+      ullamcorper convallis. </p>
+  </article>
 
 </body>
+
 </html>
