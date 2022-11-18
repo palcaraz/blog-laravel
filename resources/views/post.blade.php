1127fdf15397d28b3976b0051ef4b89fb116eec0
Pedro Alcaraz <palcaraz@gmail.com>
2022-11-18 01:22:50 +0100

    Estable - ficheros html dinámico

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -8,8 +8,12 @@
     <link rel="stylesheet" href="/app.css">
 </head>
 <body>
-	<?= $post ?>
-
+    <article>
+        <h1><?= $post->title; ?></h1>
+        <div>
+            <?= $post->body; ?>
+        </div>
+    </article>
 	<a href="/">Go back</a>
 
 </body>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -10,62 +10,20 @@
 </head>
 
 <body>
-  <article>
-    <h1><a href="/posts/my-first-post">My First Post</a></h1>
 
-    <p>
-      Lorem ipsum dolor sit amet, conectetur adipiscing elit. Morbi viverra
-      vehicula nisl eget blandit. Mauris hendrerit accumsan est id cursus.
-      Ut sed elit at ligula tempor porta sed sed odio. Aenean eget tortor
-      purus. Donec vulputate nisl ut arcu eleifend, non tempor neque
-      aliquam. Nunc dignissim augue eget enim vulputate, non vulputate velit
-      scelerisque. Vivamus hendrerit ipsum mi, non sagittis est tincidunt
-      quis. Aliquam sed nisl eget lectus scelerisque sagittis a consectetur
-      ex. Duis congue, massa vel tincidunt vestibulum, mi eros luctus lacus,
-      id malesuada sem dui molestie sapien. Orci varius natoque penatibus et
-      magnis dis parturient montes, nascetur ridiculus mus. Ut orci velit,
-      viverra at leo ut, convallis lobortis libero. Cras hendrerit sed
-      tortor vitae feugiat. Proin et arcu risus. Phasellus lacinia
-      ullamcorper convallis. </p>
-  </article>
+  <?php foreach ($posts as $post) : ?>
+    <article>
+      <h1>
+        <a href="/posts/<?= $post->slug; ?>"> 
+          <?= $post->title; ?>
+        </a> 
+      </h1>
 
-  <article>
-    <h1><a href="/posts/my-second-post">My Second Post</a></h1>
-
-    <p>
-      Lorem ipsum dolor sit amet, conectetur adipiscing elit. Morbi viverra
-      vehicula nisl eget blandit. Mauris hendrerit accumsan est id cursus.
-      Ut sed elit at ligula tempor porta sed sed odio. Aenean eget tortor
-      purus. Donec vulputate nisl ut arcu eleifend, non tempor neque
-      aliquam. Nunc dignissim augue eget enim vulputate, non vulputate velit
-      scelerisque. Vivamus hendrerit ipsum mi, non sagittis est tincidunt
-      quis. Aliquam sed nisl eget lectus scelerisque sagittis a consectetur
-      ex. Duis congue, massa vel tincidunt vestibulum, mi eros luctus lacus,
-      id malesuada sem dui molestie sapien. Orci varius natoque penatibus et
-      magnis dis parturient montes, nascetur ridiculus mus. Ut orci velit,
-      viverra at leo ut, convallis lobortis libero. Cras hendrerit sed
-      tortor vitae feugiat. Proin et arcu risus. Phasellus lacinia
-      ullamcorper convallis. </p>
-  </article>
-
-  <article>
-    <h1><a href="/posts/my-third-post">My Third Post</a></h1>
-
-    <p>
-      Lorem ipsum dolor sit amet, conectetur adipiscing elit. Morbi viverra
-      vehicula nisl eget blandit. Mauris hendrerit accumsan est id cursus.
-      Ut sed elit at ligula tempor porta sed sed odio. Aenean eget tortor
-      purus. Donec vulputate nisl ut arcu eleifend, non tempor neque
-      aliquam. Nunc dignissim augue eget enim vulputate, non vulputate velit
-      scelerisque. Vivamus hendrerit ipsum mi, non sagittis est tincidunt
-      quis. Aliquam sed nisl eget lectus scelerisque sagittis a consectetur
-      ex. Duis congue, massa vel tincidunt vestibulum, mi eros luctus lacus,
-      id malesuada sem dui molestie sapien. Orci varius natoque penatibus et
-      magnis dis parturient montes, nascetur ridiculus mus. Ut orci velit,
-      viverra at leo ut, convallis lobortis libero. Cras hendrerit sed
-      tortor vitae feugiat. Proin et arcu risus. Phasellus lacinia
-      ullamcorper convallis. </p>
-  </article>
+      <div>
+        <?= $post->excerpt; ?>
+      </div>
+    </article>
+  <?php endforeach; ?>
 
 </body>
 
