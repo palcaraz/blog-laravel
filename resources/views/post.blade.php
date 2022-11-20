a6629726568d24d1bf71a03a20f414ebba74663e
Pedro Alcaraz <palcaraz@gmail.com>
2022-11-20 01:47:17 +0100

    Estable - layout Blade con @yield

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,20 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=1024, initial-scale=1.0">
-    <title>My Blog</title>
-    <link rel="stylesheet" href="/app.css">
-</head>
-<body>
+@extends('layout')
+
+@section('content')
     <article>
-        <h1><?= $post->title; ?></h1>
+        <h1>{{ $post->title }}</h1>
         <div>
-            <?= $post->body; ?>
+            {!! $post->body !!}
         </div>
     </article>
 	<a href="/">Go back</a>
-
-</body>
-</html>
+@endsection
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,30 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layout')
 
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=1024, initial-scale=1.0">
-  <title>My Blog</title>
-  <link rel="stylesheet" href="/app.css">
-</head>
+@section('content')
 
-<body>
-
-  <?php foreach ($posts as $post) : ?>
+  @foreach ($posts as $post) 
     <article>
       <h1>
-        <a href="/posts/<?= $post->slug; ?>"> 
-          <?= $post->title; ?>
+        <a href="/posts/{{ $post->slug }}"> 
+          {{ $post->title }}
         </a> 
       </h1>
 
       <div>
-        <?= $post->excerpt; ?>
+        {{ $post->excerpt }}
       </div>
     </article>
-  <?php endforeach; ?>
-
-</body>
-
-</html>
+  @endforeach
+@endsection
